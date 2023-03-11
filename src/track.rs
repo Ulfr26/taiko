6148390685e7cb8e3e529edd84c301013db9905a
Luna Borella <ulfrthewolf@gmail.com>
2023-03-12 00:44:40 +1000

    Added some data types. About to make branches so this commit is just to save progress.

--- src/track.rs
+++ src/track.rs
@@ -5,12 +5,33 @@ pub struct TrackMetadata {
     pub offset: f32,
     pub demostart: f32,
     pub subtitle: Option<String>,
-    pub scoremode: Option<u8>,
-    pub lyrics: Option<String>,
     pub genre: Option<String>,
     pub maker: Option<String>,
 }
 
 pub struct Track {
     pub metadata: TrackMetadata,
+}
+
+#[derive(PartialEq, Eq, Ord, PartialOrd)]
+pub enum Difficulty {
+    Easy,
+    Normal,
+    Hard,
+    Oni,
+    Ura, 
+}
+
+pub enum NoteType {
+    Don,
+    Ka,
+    BigDon,
+    BigKa,
+    DrumRoll(f32),
+    BigDrumRoll(f32),
+    Balloon(f32),
+}
+
+pub struct Course {
+    pub difficulty: Difficulty,
 }
