d67fe5c51b474f049dc38d47c0038790c0dfddab
Luna Borella <ulfrthewolf@gmail.com>
2023-03-13 11:24:19 +1000

    Added basic tutorial sdl code, nothing yet.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::time::Duration;
+use sdl2::{pixels::Color, event::Event, keyboard::Keycode};
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: Eventually handle errors more gracefully.
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+
+    let window = video_subsystem.window("Taiko", 1280, 720)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(80, 80, 200));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+
+    'running: loop {
+        canvas.clear();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
+                    break 'running;
+                },
+                _ => {}
+            }
+        }
+
+        canvas.present();
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
